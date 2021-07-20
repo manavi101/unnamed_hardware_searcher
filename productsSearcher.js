43aa5fefb1470da69e58fd52080b1d8b0afc189c
manavi101 <you@example.com>
2021-07-20 15:59:57 -0300

    Recursive function for internal keys

--- productsSearcher.js
+++ productsSearcher.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
-const { exit } = require('process')
+const { exit } = require('process');
 
 function parseLocaleNumber(stringNumber, locale) {
   var thousandSeparator = Intl.NumberFormat(locale).format(11111).replace(/\p{Number}/gu, '');
@@ -17,9 +17,8 @@ const DEFAULT_VIEWPORT = {
   deviceScaleFactor: 1,
 };
 
-
 async function  productsSearcher(search,sites) { 
-	const browser = await puppeteer.launch({headless:true,defaultViewport: DEFAULT_VIEWPORT})
+	const browser = await puppeteer.launch({headless:false,defaultViewport: DEFAULT_VIEWPORT})
   const context = await browser.createIncognitoBrowserContext();
   let products = [];
   const promises = sites.map(async (v) =>{
@@ -27,30 +26,45 @@ async function  productsSearcher(search,sites) {
       const site = v;
       const page = await context.newPage()
       await page.goto(site.url,{waitUntil:['domcontentloaded', 'networkidle2']})
-      switch (site.searchType){
+      switch (site.search.type){
         case "typing":
-          await page.waitForSelector(site.searchBar)
-          await page.waitForSelector(site.searchBarButton)
-          await page.type(site.searchBar, search, {delay: 20})
-          await page.click(site.searchBarButton, {delay: 20})
+          await page.waitForSelector(site.search.searchBar)
+          await page.waitForSelector(site.search.searchBarButton)
+          await page.type(site.search.searchBar, search, {delay: 20})
+          await page.click(site.search.searchBarButton, {delay: 20})
         break;
         case "input":
           await page.evaluate ((site,search) => {
-            const searchBar = document.querySelector(site.searchBar)
-            const searchBarButton = document.querySelector(site.searchBarButton)
+            const searchBar = document.querySelector(site.search.searchBar)
+            const searchBarButton = document.querySelector(site.search.searchBarButton)
             searchBar.value = search;
             searchBarButton.click()
           },site,search)
       }
-      if(site.searchRedirect){
+      if(site.search.redirect){
         await page.waitForNavigation({waitUntil:'networkidle0'})
       }else{
         await page.waitForTimeout(2000)
       }
       const result = (await page.evaluate ((site) => {
+        function readKey(element,key,i=0){
+          if(!Array.isArray(key))
+            key = key.split(">")
+          if(element[key[i]]){
+            element = element[key[i]]
+            i++
+            if(key[i]){
+              element = readKey(element,key,i)
+            }else{
+              return element
+            }
+          }else{
+            return null
+          }
+          return element
+        }
         const tmp = {};
         const keys = site.key;
-        const substrs = site.substr;
         tmp.names = Array.from(document.querySelectorAll(site.name));
         tmp.prices = Array.from(document.querySelectorAll(site.price));
         tmp.imgs = Array.from(document.querySelectorAll(site.img));
@@ -58,21 +72,22 @@ async function  productsSearcher(search,sites) {
         tmp.stockAvailable = Array.from(document.querySelectorAll(site.stockAvailable));
         return tmp.names.map((v,i)=>{
           return {
-            name: v[keys.name],
-            price: tmp.prices[i][keys.price].substr(substrs.price.first,tmp.prices[i][keys.price].length-substrs.price.final-substrs.price.first),
-            img: tmp.imgs[i][keys.img],
-            productUrl: tmp.productUrls[i][keys.productUrl],
-            stockAvailable: (tmp.stockAvailable[i][keys.stockAvailable.key]===keys.stockAvailable.value),
+            name:       readKey(v,keys.name),
+            price:      readKey(tmp.prices[i],keys.price),
+            img:        readKey(tmp.imgs[i],keys.img),
+            productUrl: readKey(tmp.productUrls[i],keys.productUrl),
+            stockAvailable: (readKey(tmp.stockAvailable[i],keys.stockAvailable.key)===keys.stockAvailable.value)
           }
         })
       },site))
+      const substrs = site.substr;
       result.map((v)=>{
+        v.price = v.price.substr(substrs.price.first,v.price.length-substrs.price.final-substrs.price.first)
         v.price = parseLocaleNumber(v.price,site.priceFormat)
         return v;
       })
       products = products.concat(result)
       await page.close()
-      //console.log(2)
       }catch(err){
         console.log(err);
         await browser.close()
